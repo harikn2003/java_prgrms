f429af21689bc5fa46df345592a325a5bdc4f1fe
Harikrishnan N <84056391+harikn2003@users.noreply.github.com>
2025-02-26 15:52:07 +0530

    Add files via upload

---
+++ Prg1_Rectangle.java
@@ -0,0 +1,16 @@
+package java_prgrms;
+import java.util.Scanner;
+
+public class Prg1_Rectangle {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length and breadth of the Rectangle : ");
+		double length = sc.nextDouble();
+		double width = sc.nextDouble();
+		double  area = length*width;
+		double perimeter = 2*(length+width);
+		System.out.println("The Area of Rectangle is : "+ area);
+		System.out.println("The Perimeter of Rectangle is : "+ perimeter);
+	}
+
+}
---
+++ Prg2_circle.java
@@ -0,0 +1,16 @@
+package java_prgrms;
+
+import java.util.Scanner;
+
+public class Prg2_circle {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the radius of the Circle : ");
+		double radius = sc.nextDouble();
+		double area = 3.14*radius*radius;
+		double perimeter = 2*3.14*radius;
+		System.out.println("The Area of Rectangle is : "+ area);
+		System.out.println("The Perimeter of Rectangle is : "+ perimeter);
+	}
+
+}
