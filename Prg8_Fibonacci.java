a1ef42a7cb04f2f454995006508e353838587987
Harikrishnan N <84056391+harikn2003@users.noreply.github.com>
2025-03-01 10:45:01 +0530

    Add files via upload

---
+++ Prg8_Fibonacci.java
@@ -0,0 +1,22 @@
+package java_prgrms;
+import java.util.Scanner;
+
+public class Prg8_Fibonacci {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the limit : ");
+		int limit = sc.nextInt();
+			int a = 0;
+			int b = 1;
+			System.out.print(a+" "+b+" ");
+			for(int i=0;i<limit-2;i++) {
+				int c = a + b;
+				a = b;
+				b = c;
+				System.out.print(c+" ");
+			}
+			sc.close();
+		
+	}
+
+}
