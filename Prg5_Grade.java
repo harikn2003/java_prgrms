32a508a019599fbf0d7368f489a9ae48147d99ee
Harikrishnan N <84056391+harikn2003@users.noreply.github.com>
2025-03-01 10:01:42 +0530

    Update Prg5_Grade.java

--- Prg5_Grade.java
+++ Prg5_Grade.java
@@ -10,13 +10,13 @@ public class Prg5_Grade {
 		if(percent >= 90) {
 			grade = "A";
 		}
-		else if(percent >80 && percent<89) {
+		else if(percent >=80 && percent<89) {
 			grade = "B";
 		}
-		else if(percent >70 && percent<79) {
+		else if(percent >=70 && percent<79) {
 			grade = "C";
 		}
-		else if(percent >60 && percent<69) {
+		else if(percent >=60 && percent<69) {
 			grade = "D";
 		}
 		else {
